c5f9e5f847c21e9f7a49054cbb46d52fdc72d265
Edwin Hermans <edwin@madtech.cx>
2025-04-27 17:58:08 -0400

    Set up repo and add openhue-cli

---
+++ pkgs/openhue-cli/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule rec {
+  pname = "openhue-cli";
+  version = "0.16";
+
+  src = fetchFromGitHub {
+    owner = "openhue";
+    repo = "openhue-cli";
+    rev = "${version}";
+    hash = "sha256-YkY4AMCvv6IlT1xa8kpTaeTBLhAwnJkqeXWrM+LEQck=";
+  };
+
+  vendorHash = "sha256-ik7fVUZfJRTVv9ToO/S4QizEY5YWrFmRCWC1COkLLJo=";
+
+  postInstall = ''
+    mv $out/bin/openhue-cli $out/bin/openhue
+  '';
+
+  meta = with lib; {
+    description = "CLI for interacting with Philips Hue smart lighting systems";
+    homepage = "https://github.com/openhue/openhue-cli";
+    license = licenses.asl20;
+    maintainers = with maintainers; [madeddie];
+    platforms = platforms.all;
+  };
+}
