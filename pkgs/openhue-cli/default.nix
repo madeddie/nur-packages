ee5f8a468a2db183ba97da9e5deec9920d3048d8
Edwin Hermans <edwin@madtech.cx>
2025-05-07 17:51:38 -0400

    Bring flake in-line with nixpkgs flake

--- pkgs/openhue-cli/default.nix
+++ pkgs/openhue-cli/default.nix
@@ -3,29 +3,41 @@
   buildGoModule,
   fetchFromGitHub,
 }:
-buildGoModule rec {
+
+buildGoModule (finalAttrs: {
   pname = "openhue-cli";
-  version = "0.16";
+  version = "0.17";
 
   src = fetchFromGitHub {
     owner = "openhue";
-    repo = pname;
-    rev = version;
-    hash = "sha256-YkY4AMCvv6IlT1xa8kpTaeTBLhAwnJkqeXWrM+LEQck=";
+    repo = "openhue-cli";
+    rev = finalAttrs.version;
+    hash = "sha256-q+bn47cad98SAIZeR9b9w2c3uXnubIL7fa45BW9cbZ4=";
   };
 
-  vendorHash = "sha256-ik7fVUZfJRTVv9ToO/S4QizEY5YWrFmRCWC1COkLLJo=";
+  vendorHash = "sha256-lqIzmtFtkfrJSrpic79Is0yGpnLUysPQLn2lp/Mh+u4=";
+
+  env.CGO_ENABLED = 0;
+
+  rev = "f279046f988829b2a38a03f8fd4fb5cebd460dbb";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${finalAttrs.version}"
+    "-X main.commit=${finalAttrs.rev}"
+  ];
 
   postInstall = ''
     mv $out/bin/openhue-cli $out/bin/openhue
   '';
 
   meta = with lib; {
+    changelog = "https://github.com/openhue/openhue-cli/releases/tag/${finalAttrs.version}";
     description = "CLI for interacting with Philips Hue smart lighting systems";
     homepage = "https://github.com/openhue/openhue-cli";
-    license = licenses.asl20;
-    #maintainers = with maintainers; [ madeddie ];
     mainProgram = "openhue";
-    platforms = platforms.all;
+    #maintainers = with maintainers; [ madeddie ];
+    license = licenses.asl20;
   };
-}
+})
