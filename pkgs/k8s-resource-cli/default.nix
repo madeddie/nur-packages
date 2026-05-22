37b96934054845f9a59ce2a87a701ec12f125dc9
Edwin Hermans <edwin@madtech.cx>
2026-05-22 16:11:20 -0400

    Update k8s-resource-cli to latest version
    Signed-off-by: Edwin Hermans <edwin@madtech.cx>

--- pkgs/k8s-resource-cli/default.nix
+++ pkgs/k8s-resource-cli/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "k8s-resource-cli";
-  version = "0.0.5";
+  version = "0.2.1";
 
   src = fetchFromGitHub {
     owner = "madeddie";
     repo = "k8s-resource-cli";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-QpToq+W5h+PdVvKFusGxnvmunRe0wcpmLEmqIfZejLY=";
+    hash = "sha256-kfC1uy26Ke4E0EV1lB9XwVkFVq5xPiIQyPAc/BqmEY8=";
   };
 
   vendorHash = "sha256-gsu57LG318sJEfBcKMkj+XfQihVQmc+1xe0dWDFfBZM=";
